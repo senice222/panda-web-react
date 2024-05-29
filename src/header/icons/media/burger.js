4c47a05568d75324fb8b6eefb1ec19c8514baabe
pdteamlead <159976100+pdteamlead@users.noreply.github.com>
2024-05-29 18:55:28 +0200

    Delete src/header directory

--- src/header/icons/media/burger.js
+++
@@ -1,9 +0,0 @@
-export default ({fill_color}) => {
-    return (
-        <svg width="24" height="15" viewBox="0 0 24 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect x="0.285645" width="23" height="3" fill={fill_color}/>
-            <rect x="0.285645" y="6" width="17" height="3" fill={fill_color}/>
-            <rect x="0.285645" y="12" width="23" height="3" fill={fill_color}/>
-        </svg>
-    )
-}
