cef18512799cbab4157510619e961f9040e50fcb
strix3s <strix3s@archlinux.com>
2024-05-30 22:15:10 +0200

    Default

---
+++ src/components/WelcomeBlock/Welcome.jsx
@@ -0,0 +1,15 @@
+import style from './welcome.module.scss'
+
+export default () => {
+    return (
+        <section>
+            <div className={style.welcomeBlock}>
+                <p className={style.tag}>#ваш успех - в наших лапках</p>
+                <h1 className={style.title}>ДИЗАЙН</h1>
+                <h1 className={style.title}>РАЗРАБОТКА</h1>
+
+                <button className={style.button} >ОСТАВИТЬ ЗАЯВКУ</button>
+            </div>
+        </section>   
+    )
+}
